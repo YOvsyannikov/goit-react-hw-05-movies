0fed1fd08b26545760105ce63e8a3021d12472b1
Yuriy Ovsyannikov <yovsyannikov@gmail.com>
2022-10-29 16:30:17 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import Loader from './Loader/Loader';
+import Header from './Header/Header';
+import Cast from '../pages/Cast/Cast';
+import Reviews from '../pages/Reviews/Reviews';
+
+const Home = lazy(() => import('../pages/Home/Home'));
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDeails'));
+const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Header />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route exact path="/" element={<Home />}></Route>
+
+          <Route exact path="/movies" element={<Movies />}></Route>
+
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />}></Route>
+            <Route path="reviews" element={<Reviews />}></Route>
+          </Route>
+
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Suspense>
+    </>
   );
-};
+}
